8bbf5c30f2b35b8fdc1a5a3d96a2d8f826b8f4b8
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2023-12-19 12:13:21 +0100

    Final touch

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ card.addEventListener('mouseleave', () => {
     card.style.transform = 'rotateY(20deg)';
 });
 
+var audio = document.getElementById("myAudio");
+document.addEventListener('click', function() {
+    audio.play();
+    audio.muted = false;
+});
