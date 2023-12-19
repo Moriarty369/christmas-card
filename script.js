8114e5d7ac5e2ff1d40402e30ec2682597e7b1fd
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2023-12-19 09:54:33 +0100

    Adding basic project

---
+++ script.js
@@ -0,0 +1,9 @@
+const card = document.querySelector('.card');
+
+card.addEventListener('mouseenter', () => {
+    card.style.transform = 'rotateY(-20deg)';
+});
+
+card.addEventListener('mouseleave', () => {
+    card.style.transform = 'rotateY(20deg)';
+});
